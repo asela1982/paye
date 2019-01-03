8888e3606d74ee78db8ba404312e49dd7f11b38d
asela1982 <asela.d82@gmail.com>
2019-01-03 13:37:09 +0530

    add bonus module

--- app.py
+++ app.py
@@ -5,14 +5,17 @@ from flask import (
     jsonify,
     request,
     redirect,
-url_for)
+    url_for)
+
+import pandas as pd
+import pymysql
+import operator
 
 import taxpackage.salary as tx
-import taxpackage
 
 
 # Flask setup
-app = Flask(__name__) # create the flask app
+app = Flask(__name__)  # create the flask app
 
 
 @app.route("/")
@@ -23,11 +26,12 @@ def home():
 @app.route("/salary", methods=["GET", "POST"])
 def salary():
 
-    if request.method == 'POST': #this block is only entered when the form is submitted
-        income = int(request.form["incomeInput"])
-        tax = int(tx.tax_salary(income))
-        percent = "%.2f" % round((tax/income),2)
-        return render_template("salary_return.html",tax = tax, income = income, percent= percent)
+    if request.method == 'POST':  # this block is only entered when the form is submitted
+        income = float(request.form["incomeInput"])
+        print(request.form["incomeInput"])
+        tax = float(tx.tax_salary(income))
+        percent = "%.2f" % round((tax/income), 2)
+        return render_template("salary_return.html", tax=tax, income=income, percent=percent)
 
     return render_template("salary.html")
 
@@ -35,14 +39,16 @@ def salary():
 @app.route("/bonus", methods=["GET", "POST"])
 def bonus():
 
-    if request.method == 'POST': #this block is only entered when the form is submitted
-        income = int(request.form["incomeInput"])
-        bonus = int(request.form["lumpsumInput"])
-        rate = int(tx.tax_bonus(income,bonus))
-        return render_template("bonus_return.html",rate = rate)
+    if request.method == 'POST':  # this block is only entered when the form is submitted
+        income = float(request.form["incomeInput"])
+        bonus = float(request.form["lumpsumInput"])
+        rate = tx.tax_bonus(income, bonus)
+        tax = bonus*(rate/100)
+        return render_template("bonus_return.html", income=income, bonus=bonus, rate=rate, tax=tax)
 
     return render_template("bonus.html")
 
 
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=8080) #run app in debug mode in port 5000
+    # run app in debug mode in port 5000
+    app.run(debug=True, host='0.0.0.0', port=8070)
--- static/app.js
+++ static/app.js
@@ -1,16 +1,17 @@
 
-function validateForm(){
-    var fields = ["incomeInput"]
-  
-    var i, l = fields.length;
-    var fieldname;
-    for (i = 0; i < l; i++) {
-      fieldname = fields[i];
-      if (document.forms["application"][fieldname].value === "") {
-        alert(fieldname + " can not be empty");
-        return false;
-      }
+function validateForm() {
+  var fields = ["incomeInput"]
+
+  var i, l = fields.length;
+  var fieldname;
+  for (i = 0; i < l; i++) {
+    fieldname = fields[i];
+    if (document.forms["application"][fieldname].value === "") {
+      alert(fieldname + " can not be empty");
+      return false;
     }
-    return true;
-  };
-  
+  }
+  return true;
+};
+
+
